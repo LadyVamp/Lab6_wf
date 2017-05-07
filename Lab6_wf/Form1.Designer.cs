4743f2b4039bca06d74fb9a6da119f867eba80ad
LadyVamp <ladyvamp666@gmail.com>
2017-05-07 10:48:31 +0300

    Init

---
+++ Lab6_wf/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Lab6_wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtRegexp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbAction = new System.Windows.Forms.ComboBox();
+            this.cmbCompare = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(67, 143);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtRegexp
+            // 
+            this.txtRegexp.Location = new System.Drawing.Point(75, 30);
+            this.txtRegexp.Name = "txtRegexp";
+            this.txtRegexp.Size = new System.Drawing.Size(100, 20);
+            this.txtRegexp.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Regexp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Compare";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Action";
+            // 
+            // cmbAction
+            // 
+            this.cmbAction.FormattingEnabled = true;
+            this.cmbAction.Items.AddRange(new object[] {
+            "Включить",
+            "Исключить"});
+            this.cmbAction.Location = new System.Drawing.Point(75, 103);
+            this.cmbAction.Name = "cmbAction";
+            this.cmbAction.Size = new System.Drawing.Size(121, 21);
+            this.cmbAction.TabIndex = 7;
+            // 
+            // cmbCompare
+            // 
+            this.cmbCompare.FormattingEnabled = true;
+            this.cmbCompare.Items.AddRange(new object[] {
+            "Имя файла",
+            "Абсолютный путь"});
+            this.cmbCompare.Location = new System.Drawing.Point(75, 68);
+            this.cmbCompare.Name = "cmbCompare";
+            this.cmbCompare.Size = new System.Drawing.Size(121, 21);
+            this.cmbCompare.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 191);
+            this.Controls.Add(this.cmbCompare);
+            this.Controls.Add(this.cmbAction);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRegexp);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtRegexp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbAction;
+        private System.Windows.Forms.ComboBox cmbCompare;
+    }
+}
+
---
+++ Lab6_wf/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InterSystems.Data.CacheClient;
+using InterSystems.Data.CacheTypes;
+
+
+namespace Lab6_wf
+{
+    public partial class Form1 : Form
+    {
+        public CacheConnection CacheConnect;
+        public User.SearchPattern sp;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            string ConnStr = "Server = localhost;"
+                + " Port = 1972;"
+                + " Namespace = User;"
+                + " Password = 123;" //был SYS
+                + " User ID = _system;";
+
+            try
+            {
+                CacheConnect = new CacheConnection();
+                CacheConnect.ConnectionString = ConnStr;
+                CacheConnect.Open();
+            }
+            catch (Exception eLoad)
+            {
+                MessageBox.Show("An error has occurred:  " + eLoad.Message);
+            } 
+
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            User.SearchPattern sp = new User.SearchPattern(CacheConnect);
+            sp.regexp = txtRegexp.Text;
+            sp.compare = cmbCompare.Text;
+            sp.action = cmbAction.Text;
+            CacheStatus sc = sp.Save();
+            MessageBox.Show("Объект " + sp.regexp + " успешно создан!");
+        }
+    }
+}
